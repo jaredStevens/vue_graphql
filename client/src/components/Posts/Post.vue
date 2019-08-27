f7b3bf8f1a5613ab26a9dcff4a7332eb97d97c58
Jared Stevens <jared.b.stevens@gmail.com>
2019-08-27 17:16:57 -0500

    get post query

--- client/src/components/Posts/Post.vue
+++ client/src/components/Posts/Post.vue
@@ -1,14 +1,26 @@
 <template>
-  <v-container>
-    <h1>Post {{postId}}</h1>
+  <v-container v-if="getPost" class="mt-3" flexbox center>
+    <h1>{{getPost.title}}</h1>
   </v-container>
 </template>
 
 <script>
+import { GET_POST } from "../../queries"
+
 export default {
   name: "Post",
   props: [
     'postId'
-  ]
+  ],
+  apollo: {
+    getPost: {
+      query: GET_POST,
+      variables() {
+        return {
+          postId: this.postId
+        }
+      }
+    }
+  }
 }
 </script>
